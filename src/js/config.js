c87afd1eda4dd1cf099ee2dd76751feff0a047c6
pawel-pomaniec <pawelpomaniec@gmail.com>
2022-05-21 15:45:22 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 30;
+export const RES_PER_PAGE = 10;
+export const KEY = '5cf0bcf6-304b-402c-9a28-d01138cae977';
+export const MODAL_CLOSE_SEC = 3;
